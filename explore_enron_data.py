6c244dcfaec7c193eaa05c5e19c86d1f046645de
BrahmiIbtissam <ibtissambrahmi@gmail.com>
2018-09-30 16:01:06 +0100

    calculate_real_nbr_of_POIs

--- explore_enron_data.py
+++ explore_enron_data.py
@@ -24,14 +24,32 @@ print "The Enron dataset contains",len(enron_data), "people"
 
 #convert the dictionary to a dataframe
 import pandas as pd
+import glob
 df = pd.DataFrame(enron_data)
-print df.head()
-print df.shape
+#print df.head()
+#print df.shape
 
-def poi():
-    nbr=0
-    for person in enron_data.keys():
-        if enron_data[person]['poi'] == 1:
-            nbr = nbr+1
-    return nbr
-print "there are", poi(), "person of interest"
+#define a function that calculates how many persons of interest in enron_data
+#def poi():
+#    nbr=0
+#    for person in enron_data.keys():
+#        if enron_data[person]['poi'] == 1:
+#            nbr = nbr+1
+#    return nbr
+#print "there are", poi(), "person of interest"
+
+#define a function that calculates how many persons of interest in /final_project/poi_names.txt and this by reading from the txt_file and calculate the occurences
+
+
+poi_count=0
+import os
+folder = 'final_project/'
+for txt_file in os.listdir(folder):
+    with open (os.path.join(folder, txt_file)) as file:
+        for line in file.readlines():
+            for word in line.split():
+                if word=='(y)':
+                    poi_count+=1
+
+
+print "There are ",poi_count, "POIs"
